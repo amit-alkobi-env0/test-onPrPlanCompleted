0a92d03616b811b5657667df32b7aae7115d0da3
Amit Alkobi <135819599+amit-alkobi-env0@users.noreply.github.com>
2024-05-26 12:44:06 +0300

    test-pr-plan-2

--- null-resource/main.tf
+++ null-resource/main.tf
@@ -9,3 +9,9 @@ resource "null_resource" "null-test-cd-2" {
     command = "printenv"
   }
 }
+
+resource "null_resource" "null-test-pr-plan-2" {
+  provisioner "local-exec" {
+    command = "printenv"
+  }
+}
